51ba471c2545b2618c975e51e0dc840ddbf57ea8
unknown <sebaslogo@gmail.com>
2022-12-19 16:41:39 -0600

    Stochastic Gradient Descent method working (not particularly well...)

--- kinematics_2D/sgd_solver/Layers.js
+++ kinematics_2D/sgd_solver/Layers.js
@@ -1,17 +1,14 @@
 class Layer {
     
-    constructor(radius, constraints, prev) {
+    constructor(radius, constraints, theta) {
 
         this.radius = radius
         this.constraints = constraints
 
-        this.prev = prev // next layers
-        this.next = null
-
-        this.theta = Math.random()
+        this.theta = (theta === undefined) ? Math.random() : theta
+        this.matrix = this.generateMatrix(this.theta)
 
         this.prevMat = null
-        this.nextMat = null
 
     }
 
@@ -19,40 +16,43 @@ class Layer {
         this.next = nextLayer
     }
 
-    // Propagate the matrices forward
-    // A x B = C
+    /**
+     * Propagate the matrices to get a final transform representing the end-effector
+     * @param {math.matrix} prevMat The previous matrix in the product. Used for backprop.
+     * @returns The product of the previous matrix and this matrix
+     */
     forward(prevMat) {
-
         this.matrix = this.generateMatrix(this.theta)
-
-        this.prevMat = prevMat // save this matrix for the backprop step
-
+        this.prevMat = prevMat
         return math.multiply(prevMat, this.matrix)
-
     }
 
-    // Propagate the matrices backwards
-    // if A x B x C x D x E x F = G, we want to know dErr(G)/dθ, 
-    // change in Err(G) with respect to change in θ for matrix D
-    // 1. First we nudge our θ by some small d, then calculate the transform matrix for that theta (D')
-    // 2. Next we take A x B x C and transform it by D'
-    // 3. Next we take the product of A x B x C x D' and multiply it by E x F
-    // The difference in errors divided by d gives us our dErr(G)/dθ, the change in Err(G)
-    // Using this difference we can adjust the theta to descend the gradient
-    backward(nextMat) {
+    /**
+     * Propagate the matrices backwards
+     * if A x B x C x D x E x F = G, we want to know dErr(G)/dθ, 
+     * change in Err(G) with respect to change in θ for matrix D
+     * 1. First we nudge our θ by some small d, then calculate the transform matrix for that theta (D')
+     * 2. Next we take A x B x C and transform it by D'
+     * 3. Next we take the product of A x B x C x D' and multiply it by E x F
+     * The difference in errors divided by d gives us our dErr(G)/dθ, the change in Err(G)
+     * Using this difference we can adjust the theta to descend the gradient
+     * 
+     * @param {math.matrix} nextMat the E x F matrix from our example
+     * @param {function} errFn Error function
+     */
+    backward(nextMat, errFn, learnRate) {
 
         const step = 0.001
         const deltaMatrix = this.generateMatrix(this.theta + step) // get a matrix with a nudged theta
         const totalMatrix = math.multiply(this.prevMat, deltaMatrix) // get the product of all previous matrices + this matrix
         const output = math.multiply(totalMatrix, nextMat) // get the output
 
-        // get derivative from output vs target
-        // TODO: target needs to come from somewhere
-        const dTheta = getErr(output, target) / step
+        // Get dErr(END)/dθ for this joint in the arm
+        const dTheta = errFn(output) / step
 
         // nudge theta based on slope
         // TODO: better nudge
-        this.theta -= dTheta
+        this.theta -= dTheta * learnRate
 
     }
 
@@ -70,7 +70,7 @@ class Layer {
 
 class IKSystem {
 
-    ORIGIN = math.matrices([
+    ORIGIN = math.matrix([
         [1, 0, 0],
         [0, 1, 0],
         [0, 0, 1]
@@ -79,19 +79,28 @@ class IKSystem {
     constructor(radii, thetas, constraints, errFn) {
         
         this.layers = []
+        this.errFn = errFn
+
+        this.endEffector = null
+
+        this.learnRate = 0.01
 
         // initialize layers
         for (let i = 0; i < radii.length; i++) {
-            this.layers.push(new Layer(radii, constraints))
+            this.layers.push(new Layer(radii[i], constraints[i], thetas[i]))
         }
 
     }
 
-    // calculate the forward pass
-    // For A x B x C x D = E we calculate
-    // S = A x B
-    // S' = S x C
-    // E = S' x D
+    /**
+     * Calculate the forward pass
+     * For A x B x C x D = E we calculate
+     * S = A x B
+     * S' = S x C
+     * E = S' x D
+     * @param {math.matrix} origin The root transform of our arm
+     * @returns The output matrix of all the transforms
+     */
     forward(origin) {
 
         let currMat = origin
@@ -104,16 +113,54 @@ class IKSystem {
 
     }
 
-    // Perform backprop to adjust the thetas 
-    // 
+    /**
+     * Performs the backward pass on all of the transform layers of our arm.
+     * @param {math.matrix} endMat output of the forward pass (our arms simulated position)
+     */
     backward(endMat) {
 
         let nextMat = endMat
         
-        for (let i = this.layers.length - 1; i > 0; i--) {
-            this.layers[i].backward(nextMat)
+        for (let i = this.layers.length - 1; i >= 0; i--) {
+            this.layers[i].backward(nextMat, this.errFn, this.learnRate)
             nextMat = math.multiply(this.layers[i].matrix, nextMat)
         }
 
+    }
+
+    /**
+     * 
+     * @param {math.matrix} origin root transform of the arm.
+     */
+    update(origin) {
+        this.endEffector = this.forward(origin)
+        console.log(`Error: ${this.errFn(this.endEffector)}`)
+        this.backward(this.endEffector)
+    }
+
+    render(ctx, origin) {
+
+        let prevMat = origin
+
+        for (let i = 0; i < this.layers.length; i++){
+    
+            let currentMat = math.multiply(prevMat, this.layers[i].matrix)
+    
+            let startX = prevMat.get([0, 2])
+            let startY = prevMat.get([1, 2])
+    
+            let endX = currentMat.get([0, 2])
+            let endY = currentMat.get([1, 2])
+    
+            ctx.strokeStyle = "#000000"
+            ctx.lineWidth = 5
+            ctx.beginPath()
+            ctx.moveTo(startX, startY)
+            ctx.lineTo(endX, endY)
+            ctx.stroke()
+    
+            prevMat = currentMat
+    
+        }
     }
 }
---
+++ kinematics_2D/sgd_solver/main.js
@@ -0,0 +1,130 @@
+let canvas = document.getElementById("canvas")
+context = canvas.getContext("2d")
+width = canvas.width = window.innerWidth
+height = canvas.height = window.innerHeight
+
+const AXES_LENGTH = 50
+
+const X_AXIS = math.matrix([
+    [1, 0, AXES_LENGTH],
+    [0, 1, 0],
+    [0, 0, 1]
+])
+
+const Y_AXIS = math.matrix([
+    [1, 0, 0],
+    [0, 1, AXES_LENGTH],
+    [0, 0, 1]
+])
+
+const ORIGIN = math.matrix([
+    [1, 0, width / 2],
+    [0, 1, height / 2],
+    [0, 0, 1]
+])
+
+const tX = 300 + Math.random() * 400
+const tY = 300 + Math.random() * 400
+const tθ = (Math.random() - 0.5) * Math.PI * 4
+
+const TARGET = math.matrix([
+    [Math.cos(tθ), -Math.sin(tθ), tX],
+    [Math.sin(tθ), Math.cos(tθ), tY],
+    [0, 0, 1]
+])
+
+const ERROR_MARGIN = 0.5
+const RADII = [100, 75, 75, 50, 50, 25, 25, 25]
+const THETAS = [0, 0, 0, 0, 0, 0, 0, 0]
+
+// const angles = [Math.PI / 12, Math.PI] // min 15 deg, max 180 deg
+const angles = [-Math.PI, Math.PI] // full rotation
+const CONSTRAINTS = [[-Math.PI * 2, Math.PI * 2], angles, angles, angles, angles, angles, angles, angles]
+
+const PENALTY = 1000000 // penalty is so high because these configurations are NOT VALID, so the penalty needs to be huge
+
+let ikSystem = new IKSystem(RADII, THETAS, CONSTRAINTS, errFn)
+
+for (let i = 0; i < 4; i++) {
+    ikSystem.update(ORIGIN)
+}
+
+function update() {
+
+    context.clearRect(0,0,width,height)
+
+    ikSystem.update(ORIGIN)
+    ikSystem.render(context, ORIGIN)
+
+    // render end-effector
+    drawTransform(context, ikSystem.endEffector, "#000000")
+
+    // render target
+    drawTransform(context, TARGET, "#0000FF")
+
+    // setTimeout(update, 150)
+
+    // requestAnimationFrame(update)
+
+}
+
+// TODO: move these to utils.js
+function drawTransform(ctx, matrix, color) {
+
+    ctx.fillStyle = color
+
+    // Get axes relative to transform matrix
+    const startPoint = getXYfromMatrix(matrix)
+    const xAxis = getXYfromMatrix(math.multiply(matrix, X_AXIS))
+    const yAxis = getXYfromMatrix(math.multiply(matrix, Y_AXIS))
+
+    // circle
+    ctx.beginPath();
+    ctx.arc(startPoint[0], startPoint[1], 10, 0, 2 * Math.PI);
+    ctx.fill();
+
+    // draw x Axis 
+    ctx.strokeStyle = "#FF0000"
+    ctx.lineWidth = 2.5
+    ctx.beginPath()
+    ctx.moveTo(startPoint[0], startPoint[1])
+    ctx.lineTo(xAxis[0] , xAxis[1])
+    ctx.stroke()
+
+    // draw y Axis
+    ctx.strokeStyle = "#FFFF00"
+    ctx.lineWidth = 2.5
+    ctx.beginPath()
+    ctx.moveTo(startPoint[0], startPoint[1])
+    ctx.lineTo(yAxis[0] , yAxis[1])
+    ctx.stroke()
+
+}
+
+// Calculate MSE between target and end effector distance
+function getSquaredError(expected, actual) {
+
+    const errX = Math.pow(expected.get([0, 2]) - actual.get([0, 2]), 2)
+    const errY= Math.pow(expected.get([1, 2]) - actual.get([1, 2]), 2)
+
+    return (errX + errY) / 2
+
+}
+
+// Error function we pass into the solver
+function errFn(output) {
+
+    let totalErr = 0
+
+    totalErr += getSquaredError(TARGET, output)
+
+    return totalErr
+
+}
+
+// get the X and Y coords from a homogeneous transformation matrix
+function getXYfromMatrix(matrix) {
+    return [matrix.get([0, 2]), matrix.get([1, 2])]
+}
+
+update()
