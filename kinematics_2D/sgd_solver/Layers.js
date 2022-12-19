55bc8f17fd183e5d09a790b2ba389fa793862689
unknown <sebaslogo@gmail.com>
2022-12-19 16:08:30 -0600

    Added some comments for Gradient Descent method

--- kinematics_2D/sgd_solver/Layers.js
+++ kinematics_2D/sgd_solver/Layers.js
@@ -20,34 +20,40 @@ class Layer {
     }
 
     // Propagate the matrices forward
+    // A x B = C
     forward(prevMat) {
 
         this.matrix = this.generateMatrix(this.theta)
 
-        this.prevMat = prevMat
+        this.prevMat = prevMat // save this matrix for the backprop step
 
-        const newMatrix = math.multiply(prevMat, this.matrix)
-
-        this.next.forward(newMatrix)
+        return math.multiply(prevMat, this.matrix)
 
     }
 
     // Propagate the matrices backwards
+    // if A x B x C x D x E x F = G, we want to know dErr(G)/dθ, 
+    // change in Err(G) with respect to change in θ for matrix D
+    // 1. First we nudge our θ by some small d, then calculate the transform matrix for that theta (D')
+    // 2. Next we take A x B x C and transform it by D'
+    // 3. Next we take the product of A x B x C x D' and multiply it by E x F
+    // The difference in errors divided by d gives us our dErr(G)/dθ, the change in Err(G)
+    // Using this difference we can adjust the theta to descend the gradient
     backward(nextMat) {
 
-        // try nudge
         const step = 0.001
-        const deltaMatrix = this.generateMatrix(this.theta + step)
-        const output = math.multiply(nextMat, deltaMatrix)
+        const deltaMatrix = this.generateMatrix(this.theta + step) // get a matrix with a nudged theta
+        const totalMatrix = math.multiply(this.prevMat, deltaMatrix) // get the product of all previous matrices + this matrix
+        const output = math.multiply(totalMatrix, nextMat) // get the output
 
         // get derivative from output vs target
         // TODO: target needs to come from somewhere
         const dTheta = getErr(output, target) / step
 
         // nudge theta based on slope
+        // TODO: better nudge
         this.theta -= dTheta
 
-        this.prev.backward(math.multiply(this.matrix, this.nextMat)
     }
 
     // Get a matrix for the given theta
@@ -73,52 +79,41 @@ class IKSystem {
     constructor(radii, thetas, constraints, errFn) {
         
         this.layers = []
-        let prevLayer = null
 
         // initialize layers
         for (let i = 0; i < radii.length; i++) {
-            this.layers.push(new Layer(radii, constraints, prevLayer))
-            prevLayer = this.layers[i]
+            this.layers.push(new Layer(radii, constraints))
         }
 
     }
 
-    forward() {
+    // calculate the forward pass
+    // For A x B x C x D = E we calculate
+    // S = A x B
+    // S' = S x C
+    // E = S' x D
+    forward(origin) {
 
-        // might want to make forward pass controlled by the IK system?
-        this.layers[0].forward(ORIGIN)
+        let currMat = origin
 
+        for (let i = 0; i < this.layers.length; i++) {
+            currMat = this.layers[i].forward(currMat)
+        }
 
-        // for (let i = 0; i < this.layers.length; i++) {
-        //     this.layers[i].forward()
-        // }
+        return currMat
+
     }
 
-    // TODO: add control to the IKSystem class
-    // Should try to remove as much state from the Layer class as possible
-    backward() {
-        let matrices = []
+    // Perform backprop to adjust the thetas 
+    // 
+    backward(endMat) {
+
+        let nextMat = endMat
         
-        this.layers.forEach(layer => {
-            matrices.push(layer.generateMatrix(layer.theta))
-        })
-
-        const outMat = applyMatrices(this.ORIGIN, matrices)
-
-        this.layers[this.layers.length - 1].backward(outMat)
+        for (let i = this.layers.length - 1; i > 0; i--) {
+            this.layers[i].backward(nextMat)
+            nextMat = math.multiply(this.layers[i].matrix, nextMat)
+        }
 
     }
-}
-
-// apply an array of matrices to an origin matrix
-function applyMatrices(origin, matrices) {
-    
-    let curr = origin
-
-    for(let i = 0; i < matrices.length; i++){
-        curr = math.multiply(curr, matrices[i])
-    }
-
-    return curr
-
 }
