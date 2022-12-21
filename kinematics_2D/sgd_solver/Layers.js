ec8e815122ec64d3b0fd38d548998582d8fad9c9
unknown <sebaslogo@gmail.com>
2022-12-20 18:21:31 -0600

    Added skeleton for v2

--- kinematics_2D/sgd_solver/Layers.js
+++ kinematics_2D/sgd_solver/Layers.js
@@ -27,13 +27,14 @@ class Layer {
      * change in Err(G) with respect to change in θ for matrix D
      * 1. First we nudge our θ by some small d, then calculate the transform matrix for that theta (D')
      * 2. Next we take A x B x C and transform it by D'
-     * 3. Next we take the product of A x B x C x D' and multiply it by E x F
+     * 3. Next we take the product of A x B x C x D' and multiply it by E x F, which gives us the G' for D'
+     * dErr(G') - dErr(G) / d = f(x + h) - f(x) / h
      * The difference in errors divided by d gives us our dErr(G)/dθ, the change in Err(G)
      * Using this difference we can adjust the theta to descend the gradient
      * 
      * @param {math.matrix} nextMat the E x F matrix from our example
      */
-    backward(nextMat, errFn, learnRate, output) {
+    backward(nextMat, errFn, learnRate, currentLoss) {
 
         // Get the delta'd output
         const step = 0.00001
@@ -43,7 +44,7 @@ class Layer {
 
         // Get dErr(END)/dθ for this joint in the arm
         // f(x + h) - f(x) / h
-        const dTheta = (errFn(outputDelta) - errFn(output)) / step
+        const dTheta = (errFn(outputDelta) - currentLoss) / step
 
         // console.log(`dθ ${dTheta}`)
 
@@ -78,6 +79,8 @@ class IKSystem {
         
         // data
         this.layers = []
+        this.matrices = []
+        this.thetas = thetas
         this.origin = origin
         this.radii = radii
         this.armLength = radii.reduce((acc, curr) => acc + curr, 0)
@@ -136,7 +139,7 @@ class IKSystem {
         let nextMat = this.IDENTITY
         
         for (let i = this.layers.length - 1; i >= 0; i--) {
-            this.layers[i].backward(nextMat, this.error(), this.currentLearnRate, this.endEffector)
+            this.layers[i].backward(nextMat, this.error(), this.currentLearnRate, this.loss)
             nextMat = math.multiply(this.layers[i].matrix, nextMat)
         }
 
---
+++ kinematics_2D/sgd_solver_2/Solver.js
@@ -0,0 +1,113 @@
+class Solver {
+
+    constructor(radii, thetas, constraints, origin) {
+
+        this.radii = radii
+        this.thetas = thetas
+        this.constraints = constraints
+        this.origin = origin
+
+        this.matrices = []
+        this.forwardMats = []
+        this.backwardMats = []
+
+        this.endEffector = null
+        this.loss = 100.0
+        this.iterations = 0
+
+    }
+
+    // TODO: TEST THIS METHOD
+    generateMats() {
+
+        this.matrices = []
+        this.matrices.push(this.origin)
+
+        // generate all the matrices for given thetas
+        for (let i = 0; i < this.thetas.length; i++){
+            this.matrices.push(generateMatrix2D(theta, radius))
+        }
+
+        this.forwardMats = []
+        this.forwardMats.push(this.origin)
+
+        // generate all the forward partial matrix products
+        // [ O, O x A, O x A x B, O x A x B x C]
+        for (let i = 1; i < this.matrices.length; i++){
+            this.forwardMats.push(math.multiply(this.forwardMats[i - 1], this.matrices[i]))
+        }
+
+        this.backwardMats = []
+        this.backwardMats.push(this.matrices[this.matrices.length - 1])
+
+        // generate all the backwards partial matrix products
+        // [E, D x E, C x D x E] -> [C x D x E, D x E, E] 
+        for (let i = 1; i < this.matrices.length; i++){
+            this.forwardMats.push(math.multiply(this.backwardMats[i - 1], this.matrices[this.matrices.length - i - 1]))
+        }
+
+        this.backwardMats = this.backwardMats.reverse()
+
+        // this is really the forward pass error calculation
+        this.endEffector = this.forwardMats[this.forwardMats.length - 1]
+        this.loss = this.calculateLoss()
+
+    }
+
+    // TODO: TEST THIS
+    updateThetas() {
+
+        // let newThetas = []
+
+        const d = 0.00001
+
+        for (let i = 0; i < this.thetas.length; i++) {
+            const dTheta = this.thetas[i] + d
+            const radius = this.radii[i]
+            const dMat = generateMatrix2D(dTheta, radius)
+            const deltaOut = math.multiply(math.multiply(this.forwardMats[i], dMat), this.backwardMats[i+1])
+
+            const dErr = err(deltaOut) - this.loss / d
+            this.thetas[i] -= (this.momentums[i] * this.momentumRetain) + (dErr * learnRate)
+            // newThetas.push(this.thetas[i] - ((this.momentum * this.momentumRetain) + (dTheta * learnRate)))
+        }
+
+    }
+
+    update() {
+        this.generateMats()
+        this.updateThetas()
+    }
+
+    solve(target, thresh) {
+        this.iterations = 0
+
+        while (this.iterations < 250){
+            if (this.loss > thresh){
+                this.update()
+            }else{
+                console.log(`Optimal solution found after ${this.iterations} iterations!`)
+                return
+            }
+        }
+
+        console.log("Could not converge in 250 iterations")
+
+    }
+
+    // calculate error
+    calculateLoss() {
+
+    } 
+
+}
+
+function generateMatrix2D(theta, radius) {
+
+    return math.matrix([
+        [Math.cos(theta), -Math.sin(theta), radius * Math.cos(theta)],
+        [Math.sin(theta), Math.cos(theta), radius * Math.sin(theta)],
+        [0, 0, 1]
+    ])
+
+}
