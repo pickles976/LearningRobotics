bff27f42a7d3d5c8345c1dc2381f807e7385a692
unknown <sebaslogo@gmail.com>
2023-02-10 15:13:10 -0600

    Kalman Filter working for mouse tracking (not very well though)

--- Controls/KalmanFilter/main.js
+++ Controls/KalmanFilter/main.js
@@ -10,50 +10,68 @@ const ctx = canvas.getContext("2d")
 canvas.addEventListener('mousemove', (e) => {handleMouseMove(e)})
 var offsetX=canvas.offsetLeft;
 var offsetY=canvas.offsetTop;
-let mouseX, mouseY = [0,0]
-
-ctx.strokeStyle = "black"
-
-function handleMouseMove(e){
-    console.log(mouseX, mouseY)
-    ctx.beginPath()
-    ctx.moveTo(mouseX, mouseY)
-    mouseX=parseInt(e.clientX-offsetX)
-    mouseY=parseInt(e.clientY-offsetY)
-    ctx.lineTo(mouseX, mouseY)
-    ctx.stroke()
-}
+let mouseX, mouseY, mouseNoisyX, mouseNoisyY, kalmanX, kalmanY = [0,0,0,0]
 
 // Controls stuff
 const dt = 0.1
+const stdX = 3.0
+const stdY = 3.0
 
 const F = math.matrix([
-    [1, dt, 0], 
-    [0, 1, dt], 
-    [0, 0, 1]
+    [1, 0, dt, 0], 
+    [0, 1, 0, dt], 
+    [0, 0, 1, 0],
+    [0, 0, 0, 0]
 ])
 
-const H = math.matrix([[1, 0, 0]])
+const H = math.matrix([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0]
+])
+
+const t4 = dt ** 4
+const t3 = dt ** 3
+const t2 = dt ** 2
 
 const Q = math.matrix([
-    [0.05, 0.05, 0.05], 
-    [0.05, 0.05, 0.05], 
-    [0.0, 0.0, 0.0]
+    [t4/4, 0.0, t3/2, 0.0], 
+    [0.0, t4/4, 0.0, t3/2], 
+    [t3/2, 0.0, t2, 0.0],
+    [0.0, t3/2, 0.0, t2]
 ])
 
-const R = math.matrix([[0.5]])
+const R = math.matrix([
+    [stdX, 0.0],
+    [0.0, stdY]
+])
 
-// Create x and y data
-let x = linspace(-10, 10, 100)
-let measurements = x.map((val) => {
-    return val**2 + val*2 - 2 + gaussianRandom(0, 1)
-})
+const kf = new KalmanFilter(F, H, Q, R, math.matrix([0,0,0,0]))
 
-const kf = new KalmanFilter(F, H, Q, R)
-let predictions = []
 
-// smooth measurements
-measurements.forEach((z) => {
-    predictions.push(math.multiply(H, kf.predict())._data[0][0])
-    kf.update(z)
-})
+function handleMouseMove(e){
+
+    // Draw noisy line
+    ctx.strokeStyle = "black"
+    ctx.beginPath()
+    ctx.moveTo(mouseNoisyX, mouseNoisyY)
+
+    mouseX=parseInt(e.clientX-offsetX)
+    mouseY=parseInt(e.clientY-offsetY)
+    mouseNoisyX = mouseX + gaussianRandom(0,stdX)
+    mouseNoisyY = mouseY + gaussianRandom(0, stdY)
+
+    ctx.lineTo(mouseNoisyX, mouseNoisyY)
+    ctx.stroke()
+
+    // Draw filtered line
+    ctx.strokeStyle = "green"
+    ctx.beginPath()
+    ctx.moveTo(kalmanX, kalmanY)
+    let pred = math.multiply(H, kf.predict())
+    kf.update(math.matrix([mouseNoisyX, mouseNoisyY]))
+    kalmanX = pred._data[0]
+    kalmanY = pred._data[1]
+    ctx.lineTo(kalmanX, kalmanY)
+    ctx.stroke()
+
+}
