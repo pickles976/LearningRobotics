35bb3cbee6b2c71d7f9d2270942f287ac902ea51
pickles976 <sebaslogo@gmail.com>
2023-05-29 13:48:16 -0500

    getting started on bow

---
+++ OpenCV/monocular_visual_odometry_custom/main.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import cv2
+import json
+
+from lib.visualization import plotting
+from lib.visualization.video import play_trip
+
+from visual_odometry import VisualOdometry
+
+from tqdm import tqdm
+
+def main():
+    data_dir = "CUSTOM_sequence" #
+    vo = VisualOdometry(data_dir)
+
+    play_trip(vo.images)
+
+    poses = []
+    estimated_path = []
+    images=os.listdir(os.path.join(data_dir, "image_l"))
+    print(images)
+    for i, img in enumerate(tqdm(images, unit="pose")):
+        if i == 0:
+            cur_pose = np.eye(4, dtype=np.float64)
+        else:
+            q1, q2 = vo.get_matches(i)
+            transf = vo.get_pose(q1, q2) # transformation from world to camera, from extrinsic matrix
+            cur_pose = np.matmul(cur_pose, np.linalg.inv(transf)) # this calculated delta needs to be inverted to convert from camera to world
+            poses.append(cur_pose.tolist())
+       # gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
+        estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+
+    plotting.visualize_paths(estimated_path, estimated_path, "Visual Odometry", file_out=os.path.basename(data_dir) + ".html")
+    
+    # save off path to json
+    with open('poses.json', 'w') as fp:
+        json.dump({ "poses" : poses }, fp)
+
+if __name__ == "__main__":
+    main()
